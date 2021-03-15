34929e7110c03de6b7a85f5862a655724759c019
Marin Bralic <marin.bralic09@gmail.com>
2021-03-15 22:59:19 +0100

    gp

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -16,39 +16,70 @@
 #define PADS_BANK0_BASE	(APB_BASE + 0x1C000)
 
 
+#define GPIO_FUNC_XIP		0
+#define GPIO_FUNC_SPI		1
+#define GPIO_FUNC_UART	2
+#define GPIO_FUNC_I2C		3
+#define GPIO_FUNC_PWM		4
+#define GPIO_FUNC_SIO		5
+#define GPIO_FUNC_PIO0	6
+#define GPIO_FUNC_PIO1	7
+#define GPIO_FUNC_CLK		8
+#define GPIO_FUNC_USB		9
+#define GPIO_FUNC_NULL		0xf
 
-#define IO_FUNC_SPI		1
-#define IO_FUNC_UART	2
-#define IO_FUNC_I2C		3
-#define IO_FUNC_PWM		4
-#define IO_FUNC_SIO		5
-#define IO_FUNC_PIO0	6
-#define IO_FUNC_PIO1	7
-#define IO_FUNC_CLK		8
-#define IO_FUNC_USB		9
-
-
-void gpio_set_func(uint gpio_n, uint func) {
-
-
-}
-
-void gpio_set_dir(uint n, uint d) {
+#define GPIO_IRQ_LEVEL_LOW	1
+#define GPIO_IRQ_LEVEL_HIGH	2
+#define GPIO_IRQ_EDGE_FALL	4
+#define GPIO_IRQ_EDGE_RISE	8
 
 
-}
 
-void gpio_write(uint n, uint v) {
+void gpio_set_func(uint gpio, uint fn) {
 
 
 }
 
-uint gpio_read(uint gpio_n) {
+void gpio_set_pull(uint gpio, uint up, uint down) {
+
+}
+
+void gpio_set_irq(uint gpio, uint event, uint en, func callback) {
+
+}
+
+void gpio_ack_irq(uint gpio, uint event) {
+
+}
+
+void gpio_init(uint gpio, uint dir) {
+
+}
+
+void gpio_set_dir(uint n, uint out) {
+
+
+}
+
+void gpio_set(uint n, uint v) {
+
+
+}
+
+uint gpio_get(uint gpio_n) {
 	return 0;
 }
 
-void gpio_attach_intr(uint n, uint m) {
-
+uint gpio_get_all() {
 
 }
+
+uint gpio_set_all(uint mask) {
+
+}
+
+void gpio_set_dir_all(uint mask) {
+
+}
+
 
