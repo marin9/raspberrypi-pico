1aa85163ec7178fcfcc6bf0ce213a6cca1375b56
Marin Bralic <marin.bralic09@gmail.com>
2021-03-18 21:30:32 +0100

    r

---
+++ gpio/resets.c
@@ -0,0 +1,20 @@
+#include "resets.h"
+
+static voaltile uint *resets;
+
+
+void reset_block(uint block) {
+
+
+}
+
+void unreset_block(uint block) {
+
+
+}
+
+void unreset_block_wait(uint block) {
+
+
+}
+
