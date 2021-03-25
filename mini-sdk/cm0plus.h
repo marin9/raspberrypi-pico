1ba94045725a87a35532ed26ef0ab70a20213058
Marin Bralic <marin.bralic09@gmail.com>
2021-03-25 22:30:05 +0100

    c

---
+++ mini-sdk/cm0plus.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "rp2040.h"
+
+#define IRQ_TIMER0      0
+#define IRQ_TIMER1      1
+#define IRQ_TIMER2      2
+#define IRQ_TIMER3      3
+#define IRQ_PWM_WRAP    4
+#define IRQ_USBCTRL     5
+#define IRQ_XIP         6
+#define IRQ_0_PIO0      7
+#define IRQ_1_PIO0      8
+#define IRQ_0_PIO1      9
+#define IRQ_1_PIO1      10
+#define IRQ_DMA_0       11
+#define IRQ_DMA_1       12
+#define IRQ_IO_BANK0    13
+#define IRQ_IO_QSPI     14
+#define IRQ_SIO_PROC0   15
+#define IRQ_SIO_PROC1   16
+#define IRQ_CLOCKS      17
+#define IRQ_SPI0        18
+#define IRQ_SPI1        19
+#define IRQ_UART0       20
+#define IRQ_UART1       21
+#define IRQ_ADC_FIFO    22
+#define IRQ_I2C0        23
+#define IRQ_I2C1        24
+#define IRQ_RTC         25
+
+
+void cpsie();
+void cpsid();
+void svc(uint n);
+void send_event();
+void wait_event();
+void wait_intr();
--- mini-sdk/gpio.c
+++ mini-sdk/gpio.c
@@ -28,12 +28,46 @@ struct sio_hw {
 	uint gpio_oe_set;
 	uint gpio_oe_clr;
 	uint gpio_oe_xor;
-	//TODO
+	uint gpio_hi_out;
+	uint gpio_hi_out_set;
+	uint gpio_hi_out_clr;
+	uint gpio_hi_out_xor;
+	uint gpio_hi_oe;
+	uint gpio_hi_oe_set;
+	uint gpio_hi_oe_clr;
+	uint gpio_hi_oe_xor;
+	uint fifo_st;
+	uint fifo_wr;
+	uint fifo_rd;
+	uint spinlock_st;
+	uint div_udividend;
+	uint div_udivisor;
+	uint div_sdividend;
+	uint div_sdivisor;
+	uint div_quotient;
+	uint remainder;
+	uint div_csr;
 };
+
+struct splock {
+	uint spinlock[32];
+};
+
+
+#define FIFO_ST_ROE		8
+#define FIFO_ST_WOF		4
+#define FIFO_ST_RDY		2
+#define FIFO_ST_VLD		1
+
+#define DIV_SCR_DIRTY	2
+#define DIV_CSR_READY	1
+
 
 #define io		((volatile struct io_bank0_hw*)IO_BANK0_BASE)
 #define pads 	((volatile struct pads_bank0_hw*)PADS_BANK0_BASE)
 #define sio		((volatile struct sio_hw*)SIO_BASE)
+#define splock	((volatile struct splock_hw*)(SIO_BASE + 0x100))
+
 
 
 void gpio_init(uint gpio, uint fn) {
