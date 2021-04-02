013d7f7023dc66a3d99ac134d7e9d730bbff6e31
Marin Bralic <marin.bralic09@gmail.com>
2021-04-02 22:51:02 +0200

    r

--- mini-sdk/src/init.c
+++ mini-sdk/src/init.c
@@ -12,43 +12,6 @@
 #define NVIC_ICPR   (PPB_BASE + 0xE280)
 
 
-#define red(s)		gpio_set(2, (s))
-#define yellow(s)	gpio_set(3, (s))
-#define green(s)	gpio_set(4, (s))
-#define blue(s)		gpio_set(5, (s))
-
-static void leds_init() {
-	gpio_init(2, GPIO_FUNC_SIO);
-	gpio_init(3, GPIO_FUNC_SIO);
-	gpio_init(4, GPIO_FUNC_SIO);
-	gpio_init(5, GPIO_FUNC_SIO);
-	gpio_set_dir(2, 1);
-	gpio_set_dir(3, 1);
-	gpio_set_dir(4, 1);
-	gpio_set_dir(5, 1);
-	yellow(0);
-	green(0);
-	blue(0);
-	red(0);
-}
-
-static void buttons_init() {
-	gpio_init(6, GPIO_FUNC_SIO);
-	gpio_init(7, GPIO_FUNC_SIO);
-	gpio_init(8, GPIO_FUNC_SIO);
-	gpio_init(9, GPIO_FUNC_SIO);
-	gpio_set_dir(6, 0);
-	gpio_set_dir(7, 0);
-	gpio_set_dir(8, 0);
-	gpio_set_dir(9, 0);
-
-	gpio_set_pull(6, 1, 0);
-	gpio_set_pull(7, 1, 0);
-	gpio_set_pull(8, 1, 0);
-	gpio_set_pull(9, 1, 0);
-}
-
-
 void delay(int t) {
 	while (t--)
 		asm volatile ("nop");
@@ -56,7 +19,7 @@ void delay(int t) {
 
 
 void systick() {
-	uart_print("sys tick\r\n");
+	uart_print("M\r\n");
 	reg_rd(SYS_CSR);
 	//reg_wr(NVIC_ICPR, 1 << 15);
 }
@@ -100,23 +63,8 @@ gpio_init(1, GPIO_FUNC_UART);
 
 
 
-	leds_init();
-	buttons_init();
-
 reg_wr(IO_BANK0_BASE+0x100, 1<<27);
 
-	while(1) {
-		red(!gpio_get(6));
-		yellow(!gpio_get(7));
-		green(!gpio_get(8));
-		blue(!gpio_get(9));
-	}
 
-
-
-	while(1) {
-		uart_print(" B\r\n");
-		delay(1000000);
-	}
 
 }
