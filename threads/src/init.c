06eb5e76bf1dfc12514a6a688322642c29af1b25
Marin Bralic <marin.bralic09@gmail.com>
2021-04-18 12:08:36 +0200

    t

--- threads/src/init.c
+++ threads/src/init.c
@@ -5,18 +5,20 @@
 #include "thread.h"
 #include "systick.h"
 
+// -------------------------------------
+
 static sem_t sem1;
 
 static void thread_x(void *args){
 	int i;
 	while (1) {
-		//sem_wait(&sem1);
+		sem_wait(&sem1);
 		for (i = 0; i < 5; ++i) {
 			uart_print((char*)args);
-			thread_sleep(1000);
+			thread_sleep(10);
 		}
-		//sem_signal(&sem1);
-		thread_sleep(1000);
+		sem_signal(&sem1);
+		thread_sleep(10);
 	}
 }
 
@@ -30,27 +32,30 @@ static void test_sem() {
 	thread_start(thread_x, "T     4\r\n");
 }
 
+// -------------------------------------
 
 void t1() {
 	while (1) {
 		uart_print("T1\r\n");
-		thread_sleep(100);
+		thread_sleep(0);
 	}
 }
 
 void t2() {
 	while (1) {
 		uart_print("T 2\r\n");
-		thread_sleep(1000);
+		thread_sleep(0);
 	}
 }
 
 void t3() {
 	while (1) {
 		uart_print("T  3\r\n");
-		thread_sleep(2000);
+		thread_sleep(0);
 	}
 }
+
+// -------------------------------------
 
 void test_threads() {
 	thread_start(t1, 0);
@@ -58,6 +63,23 @@ void test_threads() {
 	thread_start(t3, 0);
 }
 
+
+void tm() {
+	while (1) {
+		uart_hex(rtos_ticks());
+		uart_print("\r\n");
+		thread_sleep(0);
+		uart_hex(rtos_ticks());
+		uart_print(".\r\n");
+
+	}
+}
+
+void test_time() {
+	thread_start(tm, 0);
+}
+
+// -------------------------------------
 
 void init() {
 	xosc_init();
@@ -67,7 +89,9 @@ void init() {
 
 	rtos_init();
 
-	test_sem();
+	//test_threads();
+	test_time();
+	//test_sem();
 
 	rtos_start();
 }
