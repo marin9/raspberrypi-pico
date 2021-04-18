db440868e790391c76e70712d4051f7a0927f4a2
Marin Bralic <marin.bralic09@gmail.com>
2021-04-18 11:58:44 +0200

    t

--- threads/src/init.c
+++ threads/src/init.c
@@ -5,6 +5,31 @@
 #include "thread.h"
 #include "systick.h"
 
+static sem_t sem1;
+
+static void thread_x(void *args){
+	int i;
+	while (1) {
+		//sem_wait(&sem1);
+		for (i = 0; i < 5; ++i) {
+			uart_print((char*)args);
+			thread_sleep(1000);
+		}
+		//sem_signal(&sem1);
+		thread_sleep(1000);
+	}
+}
+
+static void test_sem() {
+	sem_init(&sem1, 1);
+
+	thread_start(thread_x, "T 0\r\n");
+	thread_start(thread_x, "T  1\r\n");
+	thread_start(thread_x, "T   2\r\n");
+	thread_start(thread_x, "T    3\r\n");
+	thread_start(thread_x, "T     4\r\n");
+}
+
 
 void t1() {
 	while (1) {
@@ -27,6 +52,13 @@ void t3() {
 	}
 }
 
+void test_threads() {
+	thread_start(t1, 0);
+	thread_start(t2, 0);
+	thread_start(t3, 0);
+}
+
+
 void init() {
 	xosc_init();
 	uart_init();
@@ -35,9 +67,7 @@ void init() {
 
 	rtos_init();
 
-	thread_start(t1, 0);
-	thread_start(t2, 0);
-	thread_start(t3, 0);
+	test_sem();
 
 	rtos_start();
 }
